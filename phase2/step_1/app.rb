750b4ec3add96b71781780b11709504369ccdd8d
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-02 14:13:00 +0100

    reset file to do exercises

--- phase2/step_1/app.rb
+++ phase2/step_1/app.rb
@@ -1,31 +1,6 @@
 require 'sinatra/base'
 
 class Application < Sinatra::Base
-  # GET /
-  # root path
-  get '/hello' do
-
-    name = params[:name]
-    return "Hello #{name}"
-  end
-
-  get '/' do
-
-    return 'Hello!'
-  end
   
-  get '/posts' do
-    name = params[:name]
-    cohort_name = params[:cohort_name]
-
-    p name
-    p cohort_name
-    return "Hello #{name}, you are in the cohort #{cohort_name} of posts"
-  end
-
-  post '/posts' do
-    title = params[:title]
-
-    return "Post was created with title: #{title}"
-  end
+  
 end
